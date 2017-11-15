967a829fc7e91964b822fe3ba77727b9475938fa
eitanacks <eitan.acks@yale.edu>
2017-11-15 16:46:29 -0500

    Day 1!!

---
+++ shaker.py
@@ -0,0 +1,38 @@
+
+class FiberShaker(object):
+    """
+    Write description here
+    """
+
+    def __init__(self):
+        """
+        This function should probably handle connecting to the arduino (set baud rate, port, etc.) using pyserial
+        """
+        print(3+3)
+
+    def on(self):
+        """
+        turns on the shaker
+        :return:
+            result code
+        """
+        return
+
+    def off(self):
+        """
+        turn off the shaker
+        :return:
+            result code
+        """
+        pass
+
+    def get_status(self):
+        """
+
+        :return:
+        """
+        pass
+
+
+shaker = FiberShaker()
+shaker.on()
