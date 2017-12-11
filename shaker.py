7fd44ff28972cf6dd6a0a4c74e27471368df60ec
eitanacks <eitan.acks@yale.edu>
2017-12-11 09:47:54 -0500

    Final Draft

--- shaker.py
+++ shaker.py
@@ -1,5 +1,7 @@
 
+
 import serial
+import serial.tools.list_ports
 import time
 
 class FiberShaker(object):
@@ -7,20 +9,28 @@ class FiberShaker(object):
     Write description here
     """
 
-    def __init__(self, port = 'COM3', baud = 9600):
+    def __init__(self, baud = 9600):
         """
-        This function should probably handle connecting to the arduino (set baud rate, port, etc.) using pyserial
+        Finds Arduino and establishes serial connection. Adapted from: stackoverflow.com/questions/24214643
         """
-        self.arduino = serial.Serial(port, baud)
+        port = [
+            p.device
+            for p in serial.tools.list_ports.comports()
+            if 'Arduino' in p.description
+        ]
+        if not port:
+            raise IOError("No Arduino Found")
+
+        self.arduino = serial.Serial(port[0],baud)
         time.sleep(2)
 
     def on(self):
         """
         turns on the shaker
-        :return:
-            result code
         """
+
         self.arduino.write(str(1).encode())
+        print("Arduino On")
 
     def off(self):
         """
@@ -29,14 +39,18 @@ class FiberShaker(object):
             result code
         """
         self.arduino.write(str(0).encode())
+        print("Arduino Off")
+
 
     def get_status(self):
         """
-
         :return:
         """
-        raise NotImplementedError
-
+        return NotImplementedError
 
 shaker = FiberShaker()
 shaker.on()
+time.sleep(2)
+print("waiting")
+time.sleep(2)
+shaker.off()
