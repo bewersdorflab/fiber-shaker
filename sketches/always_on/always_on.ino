54ba7caf2451c7b2a66287b1bb07047b46b09f99
eitanacks <33702591+eitanacks@users.noreply.github.com>
2018-03-19 15:50:58 -0400

    Update for two motors
    Arduino Uno, PWM pins 3 and 5, target voltage = 3V

--- sketches/always_on/always_on.ino
+++ sketches/always_on/always_on.ino
@@ -1,10 +1,14 @@
-int motor_pin = 2;
+int motor_pin1 = 3;
+int motor_pin2 = 5;
+
 float target_voltage = 3;
 
 void setup() {
-  pinMode(motor_pin, OUTPUT);
+  pinMode(motor_pin1, OUTPUT);
+  pinMode(motor_pin2, OUTPUT);
 }
 
 void loop() {
-  analogWrite(motor_pin, 255 * target_voltage / 5);
+  analogWrite(motor_pin1, 255 * target_voltage / 5);
+  analogWrite(motor_pin2, 255 * target_voltage / 5);
 }
