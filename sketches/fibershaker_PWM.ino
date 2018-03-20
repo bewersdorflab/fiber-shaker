b4a08e3093d6afec80ff9b8f22d7cbaa8f6eeb7f
eitanacks <33702591+eitanacks@users.noreply.github.com>
2018-03-20 15:49:00 -0400

    Fibershaker arduino code for PWM control

---
+++ sketches/fibershaker_PWM.ino
@@ -0,0 +1,43 @@
+int receivedMessage = 0;
+boolean newData = false;
+int motor_pin1 = 3;
+int motor_pin2 = 5;
+float target_voltage = 3;
+
+void setup() {
+ Serial.begin(9600);
+   pinMode(motor_pin1, OUTPUT);
+   pinMode(motor_pin2, OUTPUT);
+}
+
+void loop() {
+ recvMessage();
+ interpretMessage();
+ showNewData();
+}
+
+void recvMessage() {
+ if (Serial.available() > 0) {
+ receivedMessage = Serial.parseInt();
+}
+ }
+
+void interpretMessage() {
+  if (receivedMessage == 1) {
+    newData = true;
+  }
+  else if (receivedMessage == 0) {
+    newData = false;
+  }
+}
+
+void showNewData() {
+ if (newData == true) {
+  analogWrite(motor_pin1, 255 * target_voltage / 5);
+  analogWrite(motor_pin2, 255 * target_voltage / 5);
+ }
+ else if (newData == false) {
+ digitalWrite(motor_pin1, LOW);
+ digitalWrite(motor_pin2, LOW);
+ }
+}
